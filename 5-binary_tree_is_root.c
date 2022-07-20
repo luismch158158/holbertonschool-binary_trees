a6a4cd19742fb17c7832025c6a69f4667a88701d
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-07-20 11:09:31 -0500

    fixing betty style

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root - Checks if a given node is a root
+ * binary_tree_is_root - Checks if a given node is root
  *
- * @node: Pointer to a node of the tree
+ * @node: Pointer to a node of tree
  *
  * Return: 1 if node is root, 0 otherwise.
  */
